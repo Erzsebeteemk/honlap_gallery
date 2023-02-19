c47fa9dc877f43d929051aa8f89ad912ff502eb8
Erzsebeteemk <88439357+Erzsebeteemk@users.noreply.github.com>
2023-02-19 13:15:26 +0100

    Initial commit

---
+++ data.js
@@ -0,0 +1,116 @@
+let data0 = {
+    photo: "images/Sunrise.webp",
+    title: "Sunrise",
+    description: "Sunrise Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer id orci non leo faucibus vulputate in sit amet nibh. Duis pretium, ligula eget ullamcorper aliquet, lectus elit pulvinar dolor, sed auctor massa erat sed metus."
+};
+
+let data1 = {
+    photo: "images/WaterSkyRainbow.png",
+    title: "Water Sky Rainbow",
+    description: "Water Sky Rainbow Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer id orci non leo faucibus vulputate in sit amet nibh. Duis pretium, ligula eget ullamcorper aliquet, lectus elit pulvinar dolor, sed auctor massa erat sed metus."
+};
+
+let data2 = {
+    photo: "images/UnityWithWholeness.png",
+    title: "Unity with Wholeness Photo by Zoltan Turcsanyi",
+    description: "Unity with Wholeness Photo by Zoltan Turcsanyi   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer id orci non leo faucibus vulputate in sit amet nibh. Duis pretium, ligula eget ullamcorper aliquet, lectus elit pulvinar dolor, sed auctor massa erat sed metus."
+};
+
+let data3 = {
+    photo: "images/Truth.jpg",
+    title: "Truth",
+    description: "Truth Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer id orci non leo faucibus vulputate in sit amet nibh. Duis pretium, ligula eget ullamcorper aliquet, lectus elit pulvinar dolor, sed auctor massa erat sed metus."
+};
+
+let data4 = {
+    photo: "images/YinYangBalanceFlow.jpg",
+    title: "YinYang Balance Flow",
+    description: "YinYang Balance Flow Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer id orci non leo faucibus vulputate in sit amet nibh. Duis pretium, ligula eget ullamcorper aliquet, lectus elit pulvinar dolor, sed auctor massa erat sed metus."
+};
+
+let data5 = {
+    photo: "images/FromLove.jpg",
+    title: "From Love",
+    description: "From Love Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer id orci non leo faucibus vulputate in sit amet nibh. Duis pretium, ligula eget ullamcorper aliquet, lectus elit pulvinar dolor, sed auctor massa erat sed metus."
+};
+
+let data6 = {
+    photo: "images/Alpen.JPG",
+    title: "Alpen",
+    description: "Alpen Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer id orci non leo faucibus vulputate in sit amet nibh. Duis pretium, ligula eget ullamcorper aliquet, lectus elit pulvinar dolor, sed auctor massa erat sed metus."
+};
+
+let data7 = {
+    photo: "images/ArtOfNature.jpg",
+    title: "Art of Nature",
+    description: "Art of Nature Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer id orci non leo faucibus vulputate in sit amet nibh. Duis pretium, ligula eget ullamcorper aliquet, lectus elit pulvinar dolor, sed auctor massa erat sed metus."
+};
+
+let data8 = {
+    photo: "images/ITWorldBSE.jpg",
+    title: "IT World BSE",
+    description: "IT World BSE Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer id orci non leo faucibus vulputate in sit amet nibh. Duis pretium, ligula eget ullamcorper aliquet, lectus elit pulvinar dolor, sed auctor massa erat sed metus."
+};
+
+let data9 = {
+    photo: "images/Fruit.png",
+    title: "Fruit",
+    description: "Fruit Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer id orci non leo faucibus vulputate in sit amet nibh. Duis pretium, ligula eget ullamcorper aliquet, lectus elit pulvinar dolor, sed auctor massa erat sed metus."
+};
+
+let data10 = {
+    photo: "images/Energy.jpg",
+    title: "Energy",
+    description: "Energy Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer id orci non leo faucibus vulputate in sit amet nibh. Duis pretium, ligula eget ullamcorper aliquet, lectus elit pulvinar dolor, sed auctor massa erat sed metus."
+};
+
+let data11 = {
+    photo: "images/VitalLife.jpg",
+    title: "Vital Life",
+    description: "Vital Life Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer id orci non leo faucibus vulputate in sit amet nibh. Duis pretium, ligula eget ullamcorper aliquet, lectus elit pulvinar dolor, sed auctor massa erat sed metus."
+};
+
+let data12 = {
+    photo: "images/Universe.jpg",
+    title: "Universe",
+    description: "Universe Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer id orci non leo faucibus vulputate in sit amet nibh. Duis pretium, ligula eget ullamcorper aliquet, lectus elit pulvinar dolor, sed auctor massa erat sed metus."
+};
+
+let data13 = {
+    photo: "images/Pureness.JPG",
+    title: "Pureness",
+    description: "Pureness Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer id orci non leo faucibus vulputate in sit amet nibh. Duis pretium, ligula eget ullamcorper aliquet, lectus elit pulvinar dolor, sed auctor massa erat sed metus."
+};
+
+let data14 = {
+    photo: "images/Sunset.jpg",
+    title: "Sunset",
+    description: "Sunset Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer id orci non leo faucibus vulputate in sit amet nibh. Duis pretium, ligula eget ullamcorper aliquet, lectus elit pulvinar dolor, sed auctor massa erat sed metus."
+};
+
+let data15 = {
+    photo: "images/MoonAndVenus.jpg",
+    title: "Moon and Venus",
+    description: "Moon and Venus Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer id orci non leo faucibus vulputate in sit amet nibh. Duis pretium, ligula eget ullamcorper aliquet, lectus elit pulvinar dolor, sed auctor massa erat sed metus."
+};
+
+
+
+let kepadatoktomb = [
+    data0,
+    data1,
+    data2,
+    data3,
+    data4,
+    data5,
+    data6,
+    data7,
+    data8,
+    data9,
+    data10,
+    data11,
+    data12,
+    data13,
+    data14,
+    data15
+];
---
+++ script.js
@@ -0,0 +1,111 @@
+
+// GOMBOK
+$(document).ready(function(){
+    $("#de").click(function(){
+        $("#pers").css({"opacity": "1.0", "z-index": "1"})
+
+        // $("h1").text(`${.clicked}`)
+
+        $("h1").text("...Let Love Flow...")
+    }
+    )
+}
+)
+
+$(document).ready(function(){
+    $("#im").click(function(){
+        $("#pers").css({"opacity": "0.0", "z-index": "-1"})
+        $("h1").text("Harmonythankyou")
+
+    }
+    )
+}
+)
+
+
+
+
+
+
+
+// T: KEZDŐKÉP, -CÍM, -SZÖVEG                                     
+                                                // Kezdőkép, -cím, -szöveg   (A data.js tömb 1. elemében levő, data0 nevű objektum 3 mezőjének tartalmát tölti be.)
+                                                // $(".kep").attr("src", data0.photo)
+                                                // $(".clicked").text(data0.title)
+                                                // $(".szoveg").text(data0.description)
+                                            // F: Azért nem kell leírni a fenti sorokoat, mert a betöltőfg definiálása UTÁN!!! és NEM ELŐTTE!!! meghívta a betöltőfg-t index=0 kezdőértékkel.   
+
+// T: GENERALVÁLTOZÓK                                        
+let index =0
+let elozobetoltes = 0
+
+
+
+// T: BETÖLTŐFG
+                                            // F: 0 bemeneti értékkel indul a betöltőfüggvény + append-es függvényhez hozzátettem indulásként a data-szímozo=0- s thumbnail toggle-zását.
+                                            // F: Akár a JS végén is lehetne ezzel a klasszikus formával a fg: function betoltofg(kepIndexe) { ... }
+let betoltofg = (kepIndexe) => {
+                                        console.log(elozobetoltes)
+                                        console.log(kepIndexe)
+    $(".thumbnail[data-szamozo='" + elozobetoltes + "']").toggleClass("op")
+
+    $(".kep").attr("src", kepadatoktomb[kepIndexe].photo)
+    $(".clicked").text(kepadatoktomb[kepIndexe].title)
+    $(".szoveg").text(kepadatoktomb[kepIndexe].description)
+
+    // $(".h1").text(kepadatoktomb[kepIndexe].description)
+
+
+    $(".thumbnail[data-szamozo='" + kepIndexe + "']").toggleClass("op")
+                                            // F: 2 MEGOLDÁS: Egyik megoldás: ..." + kepIndexe + "...   Másik megoldás: ` ...${kepIndexe}...`
+                                                // $(` .thumbnail[data-szamozo='${kepIndexe}'] `).toggleClass("op")
+    elozobetoltes = kepIndexe
+}
+betoltofg(index)
+
+
+
+// T: NYILAKRA KATTINTÁS
+$(".jobbra").click(() => {
+                                        console.log("jobbra kattintva")
+    index = index+1
+    if (index > kepadatoktomb.length-1) {
+        index = 0
+    }
+
+    betoltofg(index)
+}
+)
+
+$(".balra").click(() => {
+                                        console.log("balra kattintva")
+    index = index-1
+    if (index < 0) {
+        index = kepadatoktomb.length-1
+    }
+
+    betoltofg(index)
+}
+)
+
+
+
+// T: THUMBNAILEK
+                                                // Thumbnaileket betöltjük a JQuery-vel a data.js-ben tárolt tömbből, és 
+                                            // F: RÖGTÖN!!! IDE ILLESZTJÜK A THUMBNAILRE KATTINTÁS FÜGGVÉNYT IS, az a GreenFox javaslata. 
+                                            //DE: ha közvetlenül a .thumbnail lenne 1 loopban az eseményhez kötve és egyben ő lenne az eseménykezelő is, akkor csak minden 2. thumbnail-nél toggle-zna.
+                                            //Megoldás: Delegáljuk a .thumbnail parentjéhez az esemény figyelését, így még akár a .thumbnail appendje előtt is hivatkozhatunk rá.
+                                                //stackoverflow.com/posts/33392417
+kepadatoktomb.forEach((tombelem, szamol) => {
+
+    $("#keptarvalaszto").append(` <div class="thumbnail" data-szamozo="${szamol}" >   <img class="kiskep"   src="${tombelem.photo}" alt="kiskepek"> </div>  `)
+}
+)
+$(".thumbnail[data-szamozo='0']").toggleClass("op")
+
+
+$("#keptarvalaszto").on("click", ".thumbnail", function() {
+            let melyikreKattintott = $(this).attr('data-szamozo')
+            index = parseInt(melyikreKattintott)
+            betoltofg (index)
+})
